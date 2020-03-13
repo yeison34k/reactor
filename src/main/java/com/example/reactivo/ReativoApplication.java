1699c94bbf8de81b5777c7016757effb906ce118
Yeison Aristizabal <yaristizabal34k@gmail.com>
2020-03-13 16:02:15 -0500

    add project

---
+++ src/main/java/com/example/reactivo/ReativoApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactivo/ReativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
